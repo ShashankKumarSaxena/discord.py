d8bf3d1eded0c06956d5b7c365e06ebea5e06522
ShashankKumarSaxena <69804741+ShashankKumarSaxena@users.noreply.github.com>
2021-05-28 17:20:14 +0530

    uhmm

--- discord/__init__.py
+++ discord/__init__.py
@@ -9,11 +9,11 @@ A basic wrapper for the Discord API.
 
 """
 
-__title__ = 'discord'
-__author__ = 'Rapptz'
+__title__ = 'improved-discord'
+__author__ = 'Scypher'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-present Rapptz'
-__version__ = '2.0.0a'
+__version__ = '0.1.1'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
